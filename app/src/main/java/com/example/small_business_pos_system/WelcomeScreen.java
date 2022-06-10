8ceb4b00d699e1de17379842b61927fde7a319db
Marc <jmarcbalbada@gmail.com>
2022-06-10 15:36:30 +0800

    [Feature][Marc] - initial creation of android app

---
+++ app/src/main/java/com/example/small_business_pos_system/WelcomeScreen.java
@@ -0,0 +1,14 @@
+package com.example.small_business_pos_system;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
